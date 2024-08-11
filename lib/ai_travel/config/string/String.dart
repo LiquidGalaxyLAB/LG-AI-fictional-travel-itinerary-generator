63dce4913231f513bc958c095317b4b458a3e9fa
Jadu <48874687+Rohit-554@users.noreply.github.com>
2024-08-11 17:12:36 +0530

    Added the method for updating keys by user fixed bugs

--- lib/ai_travel/config/string/String.dart
+++ lib/ai_travel/config/string/String.dart
@@ -3,7 +3,9 @@ class Strings{
   static const String noCitiesAdded = "No cities added yet.";
   static const String settings = "Settings";
   static const String connectToLg = "Connect to LG";
+  static const String addKeys = "Update Key";
   static const String disconnect = "Disconnect";
+  static const String removeKeys = "Remove Keys";
   static const String doDisconnect = "Do you want to disconnect?";
   static const String connected = "Connected";
   static const String disconnected = "Disconnected";
@@ -17,6 +19,7 @@ class Strings{
   static const String pleaseUseGemma7b = "Please use gemma-7b-it for best experience";
   static const String description = "Description";
   static const String reviews = "Reviews";
+  static const String groqApiKeys = "GROQ API Keys";
 }
 
 const String gemma7b = "gemma-7b-it";
